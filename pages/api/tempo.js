4afbab9da18fc430ec17d388a43015207c2aef8f
0LostConnection <saraivasilva75@gmail.com>
2022-01-28 10:31:24 -0300

    2 files modified

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,17 @@
-function tempo(req, res) {
+async function tempo(req, res) {
   const dynamicDate = new Date();
+  const endpoint = process.env.ENDPOINT;
+
+  //const gifResponse = await fetch(
+  //  'https://api.satou-chan.xyz/api/endpoint/happy'
+  //);
+  //const gifResponseJson = await gifResponse.json();
+  //const url = gifResponseJson.url;
 
   res.json({
     date: dynamicDate.toGMTString(),
+    endpoint: endpoint,
+    //gif: url,
   });
 }
 
